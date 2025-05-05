1f12a82967053c304d5b82be8dedfa877701732c
Adam Vlček <vlcek-adam@seznam.cz>
2025-05-05 21:38:08 +0200

    added WIP .env loading

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"wlczak/shokuin/database"
 	"wlczak/shokuin/logger"
 	"wlczak/shokuin/routes/auth"
@@ -47,11 +48,56 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
+type UserRole string
+
+const (
+	Admin UserRole = "admin"
+	User  UserRole = "user"
+)
+
+// var EnvVars = []string{
+// 	"DB_HOST",
+// 	"DB_PORT",
+// 	"DB_USER",
+// 	"DB_PASSWORD",
+// 	"DB_NAME",
+// }
+
+func setupEnv() error {
+	_, err := os.Stat(".env")
+	if os.IsNotExist(err) {
+		f, err := os.Create(".env")
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+	}
+
+	// it would be nice to finish this, but it desn't have priority rn
+
+	// envMap, err := godotenv.Read(".env")
+
+	// godotenv.Read(".env")
+	// for _, value := range EnvVars {
+	// 	if envMap[value] == "" {
+	// 		godotenv.Write(map[string]string{
+	// 			value: "",
+	// 		}, ".env")
+	// 	}
+	// 	fmt.Println(value)
+	// }
+
+	err = godotenv.Load()
+
+	return err
+}
+
 func main() {
 	zap := logger.GetLogger()
 	zap.Info("Starting server")
 
-	err := godotenv.Load()
+	err := setupEnv()
+
 	if err != nil {
 		zap.Fatal(err.Error())
 	}
