14ba1a25bb4fb26039a6d13bb91d14fa6c0489ec
Adam Vlček <vlcek-adam@seznam.cz>
2025-05-17 13:07:15 +0200

    added body json reading

--- routes/api/additem.go
+++ routes/api/additem.go
@@ -6,8 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AddItem struct {
+	Name  string `json:"name"`
+	Price int32  `json:"price"`
+}
+
 func AddItemApi(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Add Item",
-	})
+
+	var additem AddItem
+	c.ShouldBindJSON(&additem)
+
+	c.JSON(http.StatusOK, additem)
+
 }
