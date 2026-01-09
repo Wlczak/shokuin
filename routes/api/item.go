6bd441decfd533dd84ef079eed41383a470188b9
Adam Vlček <vlcek-adam@seznam.cz>
2026-01-09 15:30:32 +0100

    added item get method

--- routes/api/item.go
+++ routes/api/item.go
@@ -7,13 +7,46 @@ import (
 	"wlczak/shokuin/database"
 	"wlczak/shokuin/database/schema"
 	"wlczak/shokuin/logger"
+	api_schema "wlczak/shokuin/routes/api/schema"
 	"wlczak/shokuin/routes/error_handl"
 
 	"github.com/gin-gonic/gin"
 )
 
+// GetItemApi returns the item with the given id
+// @Summary Get an item
+// @Description Returns the item with the given id
+// @Tags item
+// @Accept json
+// @Produce json
+// @Param id path string true "Item ID"
+// @Success 200 {object} api_schema.Item
+// @Failure 404 "Item not found"
+// @Router /item/{id} [get]
 func (a *ApiController) GetItemApi(c *gin.Context) {
+	id := c.Param("id")
 
+	db, err := database.GetDB()
+
+	if err != nil {
+		zap := logger.GetLogger()
+		zap.Error(err.Error())
+		panic(err)
+	}
+
+	var dbitem schema.Item
+	db.DB.Where("id = ?", id).First(&dbitem)
+
+	if dbitem.ID == 0 {
+		c.JSON(http.StatusNotFound, nil)
+		return
+	}
+	item := api_schema.Item{
+		ID:             dbitem.ID,
+		ItemTemplateId: dbitem.ItemTemplateId,
+		ExpiryDate:     dbitem.ExpiryDate,
+	}
+	c.JSON(http.StatusOK, item)
 }
 
 // AddItemApi adds a new item to the database
--- routes/api/schema/item_schema.go
+++ routes/api/schema/item_schema.go
@@ -1,6 +1,14 @@
 package api_schema
 
+import "time"
+
 type AddItem struct {
 	Name    string `json:"name"`
 	Barcode string `json:"barcode"`
+}
+
+type Item struct {
+	ID             uint      `json:"id"`
+	ItemTemplateId int       `json:"item_template_id"`
+	ExpiryDate     time.Time `json:"expiry_date"`
 }
